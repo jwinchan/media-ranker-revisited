de900dfb664f67d668541c972f60eff6ec6aa1f4
Jessica Chan <jwinchan@gmail.com>
2020-12-01 05:01:43 -0800

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+  validates :uid, uniqueness: [scope: :provider]
 end
